56debf8cf38bd7c35dee6c9719922698a4e85582
Josh Faigan <djjoker13@gmail.com>
2023-12-06 15:35:56 -0500

    added type for batch cache event submissions

--- jobs/cache_event_handler.py
+++ jobs/cache_event_handler.py
@@ -19,6 +19,7 @@ class BatchCacheEventHandler:
   async def send_aggregated_event(self):
     if self.cache_hits or self.cache_misses:
         event_data = {
+            "type": "cache",
             "cache_hits": self.cache_hits,
             "cache_misses": self.cache_misses
         }
