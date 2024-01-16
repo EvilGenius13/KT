6d93c2fe6894ed75d1c27036f8ea2f0cf3abcb95
Josh Faigan <djjoker13@gmail.com>
2024-01-15 22:09:00 -0500

    fixing local env redis connection

--- initializers/redis.py
+++ initializers/redis.py
@@ -3,8 +3,12 @@ import os
 
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
+if os.getenv('LOCAL_ENV') == 'true':
+  HOST = "localhost" 
+else:
+  HOST = "redis"
 r = redis.Redis(
-  host='localhost', 
+  host=HOST, 
   port=6379,
   password=REDIS_PASSWORD,
 )
